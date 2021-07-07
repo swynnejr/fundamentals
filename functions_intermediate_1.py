c18db8894f3fc0e457cc6aa069591cafb65f3bb8
swynnejr <saucersam@gmail.com>
2021-07-07 16:40:58 -0500

    1.1 done

---
+++ functions_intermediate_1.py
@@ -0,0 +1,23 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+#1.1 Change value 10 in x to 15
+
+# x[1][0] = 15
+# print(x)
+
+#1.2 Change the last_name of the first student from 'Jordan' to 'Bryant'
+
+
+
+#1.3 In the sports_directory, change 'Messi' to 'Andres'
+
+#1.4 Change the value 20 in z to 30
