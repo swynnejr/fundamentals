2c7386b216214e99225892a13b31860d9d13cdff
swynnejr <saucersam@gmail.com>
2021-07-06 23:43:27 -0500

    I'm terrible at functions

--- functions_basic_2.py
+++ functions_basic_2.py
@@ -1,6 +1,10 @@
 # Countdown - Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
 # Example: countdown(5) should return [5,4,3,2,1,0]
-
+newList = []
+def countdown(x):
+    for i in range (x,0,-1):
+        return newList
+print(countdown(5))
 
 # Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 # Example: print_and_return([1,2]) should print 1 and return 2
